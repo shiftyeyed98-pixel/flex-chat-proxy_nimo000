7f169fa6035d33186cb7962c47f5458fa9993c49
shiftyeyed98-pixel <shifty.eyed.98@gmail.com>
2026-04-30 21:19:03 +0900

    fetch 부분 Referer 추가

--- api/chat.js
+++ api/chat.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
     const response = await fetch(targetUrl, {
       headers: {
         "User-Agent": "Mozilla/5.0",
+        "Referer": "https://flexhp.kr/"
       },
     });
 
